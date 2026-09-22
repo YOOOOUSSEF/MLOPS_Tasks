21389f63a3aebacf1a6aa8e84cbfda003af30c35
youssef hassan <yussefhassan600@gmail.com>
2026-09-22 22:04:26 +0300

    add seed.sql utd-8 encoded to download data first so integration test can works

---
+++ task_3/tests/test_integration_pipeline.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import pytest
+import yaml
+
+from src.config import get_project_root
+from src.pipeline import InferencePipeline
+
+
+def test_invalid_order_id():
+    sample_order_id = ["test-order-001"]
+
+    with pytest.raises(ValueError, match="NO ORDER"):
+        project_root = get_project_root()
+        config = yaml.safe_load(open(project_root / "config" / "params.yaml"))
+
+        # os.chdir("tests")# you are now on d:/MlOps_tasks/task_3/tests so paths can work.
+
+        pipeline = InferencePipeline(
+            model_path=config["paths"]["mlflow_model_uri"],
+            imputer_path=Path(config["paths"]["imputer"]),
+            feature_list_path=Path(config["paths"]["feature_list"]),
+        )
+
+        print(pipeline.predict(sample_order_id))
